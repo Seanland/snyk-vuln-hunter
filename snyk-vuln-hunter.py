51d12c4cf2203654783372d613d06711bd2577c8
Sean Clarke <me@seanland.ca>
2023-07-19 15:37:42 -0400

    added some style, working for OS for the CWE and CVE

--- snyk-vuln-hunter.py
+++ snyk-vuln-hunter.py
@@ -10,6 +10,26 @@ def main():
     here = os.getcwd()
     path = sys.argv[1]
     vuln = sys.argv[2]
+
+    if not vuln.startswith(("CVE", "CWE")):
+      raise ValueError("CVE or CWE not detected.")
+    else:
+      print(f'''
+ __             _                  _                            _            
+/ _\_ __  _   _| | __ /\   /\_   _| |_ __     /\  /\_   _ _ __ | |_ ___ _ __ 
+\ \| '_ \| | | | |/ / \ \ / / | | | | '_ \   / /_/ / | | | '_ \| __/ _ \ '__|
+_\ \ | | | |_| |   <   \ V /| |_| | | | | | / __  /| |_| | | | | ||  __/ |   
+\__/_| |_|\__, |_|\_\   \_/  \__,_|_|_| |_| \/ /_/  \__,_|_| |_|\__\___|_|   
+          |___/                                                              
+
+''') 
+      hunter = "Hunting for " + vuln + "!"
+      bar = "=" * len(hunter)
+
+      print(f'''{bar}''')
+      print(f'''{hunter}''')
+      print(f'''{bar}\n''')
+
     timetag = datetime.now().strftime("%Y%m%d%H%M%S")
 
     use_snyk_os(path, here, vuln, timetag)
@@ -37,38 +57,30 @@ def use_snyk_os(app_loc, cur_loc, vuln, t):
 def os_vuln_hunter(data, vuln):
   filtered_data_sets = []
 
-  # Filter by CWE
-  # desired_cwe = "CWE-1321"
-
-  # print(data["vulnerabilities"][1]["identifiers"])
-
-  for data_set in data["vulnerabilities"]:
-    if "identifiers" in data_set:
-      # print(data_set["identifiers"])
-      if "CWE" in data_set["identifiers"] and vuln in data_set["identifiers"]["CWE"]:
-        filtered_data_sets.append(data_set)
+  filtered_data_sets = filter_os_data_set(data, vuln)
 
   # Print the filtered data sets
   for data_set in filtered_data_sets:
     print(f'''{data_set["id"]}: from package:{data_set["name"]}:{data_set["version"]} and it is upgradeable -> {data_set["isUpgradable"]} from -> {data_set["from"]} upgradepath -> {data_set["upgradePath"]} it is fixed in versions -> {data_set["fixedIn"]}\n''')
 
+def filter_os_data_set(data, vuln):
+  clean_data = []
 
+  if vuln.startswith("CWE"):
+    for data_set in data["vulnerabilities"]:
+      if "identifiers" in data_set:
+        # print(data_set["identifiers"])
+        if "CWE" in data_set["identifiers"] and vuln in data_set["identifiers"]["CWE"]:
+          clean_data.append(data_set)
 
-# def run_snyk_command(path, string):
-#     try:
-#         # Construct the command to run
-#         command = ['snyk', path, string]
+  if vuln.startswith("CVE"):
+    for data_set in data["vulnerabilities"]:
+      if "identifiers" in data_set:
+        # print(data_set["identifiers"])
+        if "CVE" in data_set["identifiers"] and vuln in data_set["identifiers"]["CVE"]:
+          clean_data.append(data_set)
 
-#         # Run the command using subprocess
-#         result = subprocess.run(command, capture_output=True, text=True)
-
-#         # Print the command output
-#         print(result.stdout)
-#         # Check for any errors
-#         if result.returncode != 0:
-#             print(f"Error: {result.stderr}", file=sys.stderr)
-#     except FileNotFoundError:
-#         print("Error: 'snyk' command not found. Please make sure it is installed and added to the system PATH.", file=sys.stderr)
+  return clean_data
 
 # Running Script
 main()
